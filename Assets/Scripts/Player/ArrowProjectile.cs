8acc3f7ccc2a9fc4f981567fe26f77f97bd43070
Dima <dskeygard@mail.ru>
2022-05-21 01:17:13 +0300

    Quiver

--- Assets/Scripts/Player/ArrowProjectile.cs
+++ Assets/Scripts/Player/ArrowProjectile.cs
@@ -20,7 +20,6 @@ public class ArrowProjectile : MonoBehaviour
         _rb.velocity = transform.forward * _bulletSpeed;
     }
 
-
     private void OnTriggerEnter(Collider other)
     {
         var rnd = Random.Range(1, 100);
@@ -37,6 +36,11 @@ public class ArrowProjectile : MonoBehaviour
                 enemyStats.CalculateDamage(CurrentDamage.CurrentDamageS.CurrentWeaponPhysDamage, DamageType.PhysDamage);
             }
         }
+        if (other.TryGetComponent(out DestroyShards _destroyShards))
+        {
+            _destroyShards.ShardsDestroy();
+            transform.SetParent(other.transform);
+        }
         else
         {
             transform.SetParent(other.transform);
@@ -44,7 +48,7 @@ public class ArrowProjectile : MonoBehaviour
         _rb.velocity = Vector3.zero;
         _rb.angularVelocity = Vector3.zero;
         _rb.isKinematic = true;
-        
+
         _collider.enabled = false;
         Destroy(gameObject, _lifeTime);
     }
